271480dd5cb3919c532c530dcf3b902d5d98b083
Alex <Alex@DESKTOP-QSN4B7E>
2020-04-23 21:43:07 +1200

    created basic structure

--- src/Farm.java
+++ src/Farm.java
@@ -1,4 +1,77 @@
+import java.util.Scanner;
 
 public class Farm {
-	//uh yeet
+	
+	String name;
+	Farmer farmer;
+	int gameLength,day= 0, farmType, growthMod = 1, happinessMod = 1, incomeMod = 1;
+	boolean storeDiscount;
+	
+	public Farm (String name, String farmerName) {
+		this.name = name;
+		this.farmer = new Farmer(farmerName);
+	}
+	
+	public void printFarm () {
+		System.out.println("This is farm " + name + " and it is occupied by farmer " + farmer.getName());
+	}
+	
+	public void nextDay() {
+		this.day += 1;
+		if (this.day == this.gameLength) {
+			this.endGame();
+		}
+	}
+	
+	public void endGame() {
+		System.out.println("You ended the game with " + calculatePoints() + " points.");
+	}
+	
+	public int calculatePoints() {
+		return 69420;
+	}
+	
+	public static Farm setUpFarm(Scanner in) {
+		System.out.println("Enter farm name:");
+		String farmName = in.nextLine();
+		System.out.println("Enter farmer name:");
+		String farmerName = in.nextLine();
+		System.out.println("What type of farm do you want to start with? ((0,Humble), Range, Field, (3,Wealthy))");
+		int farmType = in.nextInt();
+		Farm farm = new Farm(farmName, farmerName);
+		switch (farmType) {
+			case 0:
+				farm.storeDiscount = true;
+				break;
+			case 1:
+				farm.happinessMod += 1;
+				farm.growthMod -= 1;
+				farm.storeDiscount = false;
+				break;
+			case 2:
+				farm.happinessMod -= 1;
+				farm.growthMod += 1;
+				farm.storeDiscount = false;
+				break;
+			case 3:
+				farm.incomeMod += 1;
+				farm.storeDiscount = false;
+			default:
+				farmType = 0;
+				break;
+		}
+		farm.printFarm();
+		System.out.println("How long should this game last?");
+		farm.gameLength = in.nextInt();
+		return farm;
+	}
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		farm = setUpFarm(in);
+		
+		in.close();
+		
+	}
+	
 }
---
+++ src/Farmer.java
@@ -0,0 +1,13 @@
+
+public class Farmer {
+	
+	private String name;
+	
+	public Farmer (String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+}

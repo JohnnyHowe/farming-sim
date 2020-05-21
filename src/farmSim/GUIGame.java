284ab7b972ef56cfe3c94671c2046118db379203
Johnny Howe <jonathon.o.howe@gmail.com>
2020-05-22 08:33:58 +1200

    scaleUp
    + GUIGame.scaleUp added because I don't wanna learn how to use swing properly

--- src/farmSim/GUIGame.java
+++ src/farmSim/GUIGame.java
@@ -9,7 +9,9 @@ import java.awt.*;
  */
 public class GUIGame extends Game {
 
-    JFrame window;
+    JFrame frame;
+    int frameX = 600;
+    int frameY = 600;
 
     public GUIGame() {
         super();
@@ -17,17 +19,44 @@ public class GUIGame extends Game {
     }
 
     private void setUpWindow() {
-        this.window = new JFrame("Farming Simulator!");
-        this.window.setSize(600, 600);
-        this.window.setVisible(true);
+        this.frame = new JFrame("Farming Simulator!");
+        this.frame.setSize(this.frameX, this.frameY);
+        this.frame.setLayout(null);
+        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        this.frame.setVisible(true);
     }
 
     private void intoScreen() {
         JLabel title = new JLabel("Welcome to Farming Simulator!", JLabel.CENTER);
-        Font titleFont = new Font(title.getFont().getName(), Font.PLAIN, 40);
-        title.setVerticalAlignment(SwingConstants.TOP);
+        Font titleFont = new Font(title.getFont().getName(), Font.PLAIN, 35);
         title.setFont(titleFont);
-        this.window.add(title);
+        this.scaleUp(title, 0.5f, 0.1f, 1f, 0.2f);
+        this.frame.add(title);
+
+        JButton continueButton = new JButton("Next");
+        this.scaleUp(continueButton, 0.5f, 0.8f, 0.5f, 0.1f);
+        this.frame.add(continueButton);
+
+        this.frame.setVisible(true);
+    }
+
+    /**
+     * Given a component and a rect scale it up.
+     * x, y, w, h are all relative to the window size - between 0 and 1
+     * So to centre a button on the screen, it's x and y would be 0.5f.
+     * NOTE: (0, 0) is top left of the screen.
+     * @param component component to scale up
+     * @param x centre x position to place component
+     * @param y centre y position to place component
+     * @param w width to set component to
+     * @param h height to set component to
+     */
+    private void scaleUp(JComponent component, float x, float y, float w, float h) {
+        int sx = Math.round((x - w / 2) * this.frameX);
+        int sy = Math.round((y - h / 2) * this.frameY);
+        int sw = Math.round(w * this.frameX);
+        int sh = Math.round(h * this.frameY);
+        component.setBounds(sx, sy, sw, sh);
     }
 
     public void runDay() {

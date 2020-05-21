7efddecb896b9c85eda8561d7eb6da75dafeaf78
Johnny Howe <jonathon.o.howe@gmail.com>
2020-05-22 10:31:27 +1200

    Window resizing!
    All is good and well apart from windowSizeChanged not working unless it's go a print statement in it.
    
    idk man

--- src/farmSim/GUIGame.java
+++ src/farmSim/GUIGame.java
@@ -12,10 +12,27 @@ public class GUIGame extends Game {
     JFrame frame;
     int frameX = 600;
     int frameY = 600;
+    int frameScale = 600;    // How objects are scaled - min(x, y)
 
     public GUIGame() {
         super();
         this.setUpWindow();
+    }
+
+    /**
+     * Has the size of the window changed since the last frame?
+     * To be called once per loop/frame
+     * Sets frameX, frameY and frameScale
+     * @return window size change
+     */
+    private boolean windowSizeChanged() {
+        Dimension size = frame.getContentPane().getSize();
+        boolean change = size.width != this.frameX || size.height != this.frameY;
+        this.frameX = size.width;
+        this.frameY = size.height;
+        this.frameScale = Math.min(this.frameX, this.frameY);
+        System.out.println(change);
+        return change;
     }
 
     private void setUpWindow() {
@@ -27,6 +44,8 @@ public class GUIGame extends Game {
     }
 
     private void intoScreen() {
+        this.clearFrame();
+
         JLabel title = new JLabel("Welcome to Farming Simulator!", JLabel.CENTER);
         Font titleFont = new Font(title.getFont().getName(), Font.PLAIN, 35);
         title.setFont(titleFont);
@@ -37,6 +56,14 @@ public class GUIGame extends Game {
         this.scaleUp(continueButton, 0.5f, 0.8f, 0.5f, 0.1f);
         this.frame.add(continueButton);
 
+        JLabel nameLabel = new JLabel("Name:");
+        this.scaleUp(nameLabel, 0.4f, 0.6f, 0.2f, 0.1f);
+        this.frame.add(nameLabel);
+
+        JTextField nameField = new JTextField();
+        this.scaleUp(nameField, 0.6f, 0.6f, 0.2f, 0.1f);
+        this.frame.add(nameField);
+
         this.frame.setVisible(true);
     }
 
@@ -44,7 +71,7 @@ public class GUIGame extends Game {
      * Given a component and a rect scale it up.
      * x, y, w, h are all relative to the window size - between 0 and 1
      * So to centre a button on the screen, it's x and y would be 0.5f.
-     * NOTE: (0, 0) is top left of the screen.
+     * NOTE: The game is scaled st it is a square in the middle of the rectangle window
      * @param component component to scale up
      * @param x centre x position to place component
      * @param y centre y position to place component
@@ -52,20 +79,40 @@ public class GUIGame extends Game {
      * @param h height to set component to
      */
     private void scaleUp(JComponent component, float x, float y, float w, float h) {
-        int sx = Math.round((x - w / 2) * this.frameX);
-        int sy = Math.round((y - h / 2) * this.frameY);
-        int sw = Math.round(w * this.frameX);
-        int sh = Math.round(h * this.frameY);
-        component.setBounds(sx, sy, sw, sh);
+        // Centered unscaled
+        float cux = x - w / 2;
+        float cuy = y - h / 2;
+        // Centered pos (on window)
+        float cx = (cux - 0.5f) * this.frameScale;
+        float cy = (cuy - 0.5f) * this.frameScale;
+        // Screen center (in pixels)
+        float scx = ((float) this.frameX) / 2;
+        float scy = ((float) this.frameY) / 2;
+        // Pos to put things
+        float sx = scx + cx;
+        float sy = scy + cy;
+        // size of things
+        float sw = w * this.frameScale;
+        float sh = h * this.frameScale;
+        component.setBounds(Math.round(sx), Math.round(sy), Math.round(sw), Math.round(sh));
+    }
+
+    private void clearFrame() {
+        this.frame.getContentPane().removeAll();
+        this.frame.repaint();
     }
 
     public void runDay() {
-        System.out.println("Running day in GUI");
+//        System.out.println("Running day in GUI");
+        if (this.windowSizeChanged()) {
+            this.intoScreen();
+        }
     }
 
     public static void main(String[] args) {
         GUIGame game = new GUIGame();
+        game.setGameLength(10);
         game.intoScreen();
-//        game.run();
+        game.run();
     }
 }

615c870c28b2ce95722a5eccec2435463edf03e0
Johnny Howe <jonathon.o.howe@gmail.com>
2020-05-25 11:18:59 +1200

    Store item selection in

--- src/game/guiGame/StoreScreen.java
+++ src/game/guiGame/StoreScreen.java
@@ -1,61 +1,83 @@
 package game.guiGame;
 
+import animals.Animal;
 import crops.*;
+import farm.Farm;
+import farm.FarmItem;
+import farm.ItemFactory;
 import game.Game;
+import items.Item;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.*;
 
 public class StoreScreen {
     private JPanel panel1;
     private JLabel storeLabel;
-    private JButton buyWheatButton;
-    private JButton buyMelonButton;
-    private JButton buyCactusButton;
-    private JButton buySugarCaneButton;
-    private JButton buyPumpkinButton;
-    private JButton buyMushroomButton;
+    private JButton nextButton;
+    private JButton previousButton;
+    private JLabel itemNumberLabel;
+    private JLabel itemNameLabel;
 
     private JFrame frame;
+    private int currentSlot = 0;
 
     public StoreScreen() {
-        buyWheatButton.addActionListener(new ActionListener() {
+        previousButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                Game.getFarm().addFarmItem(new Wheat());
+                changeSelectedSlot(-1);
+                updateItemNumberLabel();
+                updateCurrentItem();
             }
         });
-        buyMushroomButton.addActionListener(new ActionListener() {
+        nextButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                Game.getFarm().addFarmItem(new Mushroom());
+                changeSelectedSlot(1);
+                updateItemNumberLabel();
+                updateCurrentItem();
             }
         });
-        buyPumpkinButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                Game.getFarm().addFarmItem(new Pumpkin());
-            }
-        });
-        buySugarCaneButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                Game.getFarm().addFarmItem(new SugarCane());
-            }
-        });
-        buyCactusButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                Game.getFarm().addFarmItem(new Cactus());
-            }
-        });
-        buyMelonButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                Game.getFarm().addFarmItem(new Melon());
-            }
-        });
+    }
+
+    private void updateCurrentItem() {
+        List<Crop> crops = Collections.list(ItemFactory.GetAllCrops().elements());
+        List<Animal> animals = Collections.list(ItemFactory.GetAllAnimals().elements());
+        List<Item> items = Collections.list(ItemFactory.GetAllItems().elements());
+        if (currentSlot < crops.size()) {
+            itemNameLabel.setText(crops.get(currentSlot).getName());
+        } else if (currentSlot < crops.size() + animals.size()) {
+            itemNameLabel.setText(animals.get(currentSlot - crops.size()).getName());
+        } else {
+            itemNameLabel.setText(items.get(currentSlot - crops.size() - animals.size()).getName());
+        }
+    }
+
+    /**
+     * Increase the selected slot number by change
+     * Clamps it between 0 and 14 (inclusive)
+     * @param change
+     */
+    private void changeSelectedSlot(int change) {
+        currentSlot = Math.max(Math.min(currentSlot + change, 14), 0);
+        previousButton.setEnabled(true);
+        nextButton.setEnabled(true);
+        if (currentSlot == 0) {
+            previousButton.setEnabled(false);
+        } else if (currentSlot == 14) {
+            nextButton.setEnabled(false);
+        }
+    }
+
+    /**
+     * Update the item number label to
+     * "Showing Paddock Slow currentslot+1/9"
+     */
+    private void updateItemNumberLabel() {
+        itemNumberLabel.setText("Showing Item " + (currentSlot + 1) + "/x");
     }
 
     public static void make() {
@@ -64,5 +86,10 @@ public class StoreScreen {
         screen.frame.setContentPane(screen.panel1);
         screen.frame.setVisible(true);
         screen.frame.pack();
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+        itemNameLabel = new JLabel();
     }
 }

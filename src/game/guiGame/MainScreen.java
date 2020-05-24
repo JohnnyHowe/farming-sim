45f1aee031a0b37bca71d5a7dedee6abd79b8660
Johnny Howe <jonathon.o.howe@gmail.com>
2020-05-24 20:41:01 +1200

    Started new inventory thingy

--- src/game/guiGame/MainScreen.java
+++ src/game/guiGame/MainScreen.java
@@ -26,50 +26,15 @@ public class MainScreen {
     private JLabel farmerInfoLabel;
     private JLabel actionsLeftLabel;
     private JPanel paddockPanelContainer;
+    private JButton previousButton;
+    private JButton nextButton;
+    private JLabel itemNumberLabel;
 
     private JPanel paddockPanel;
-    private ArrayList<JPanel> paddockPanels;
-    private JPanel paddockPanel0;
-    private JPanel paddockPanel1;
-    private JPanel paddockPanel2;
-    private JPanel paddockPanel3;
-    private JPanel paddockPanel4;
-    private JPanel paddockPanel5;
-    private JPanel paddockPanel6;
-    private JPanel paddockPanel7;
-    private JPanel paddockPanel8;
 
-//    private enum paddockSlots {
-//        SLOT0, SLOT1, SLOT2, SLOT3, SLOT4, SLOT5, SLOT6, SLOT7, SLOT8;
-//    }
-//
-//    private JPanel getPaddockPanel(paddockSlots slot) {
-//        switch (slot) {
-//            case SLOT0: return paddockPanel0;
-//            case SLOT1: return paddockPanel1;
-//            case SLOT2: return paddockPanel2;
-//            case SLOT3: return paddockPanel3;
-//            case SLOT4: return paddockPanel4;
-//            case SLOT5: return paddockPanel5;
-//            case SLOT6: return paddockPanel6;
-//            case SLOT7: return paddockPanel7;
-//            case SLOT8: return paddockPanel8;
-//            default: return paddockPanel0;  // TODO write exception
-//        }
-//    }
+    private int currentSlot = 0;
 
     public MainScreen() {
-
-        paddockPanels = new ArrayList<JPanel>();
-        paddockPanels.add(paddockPanel0);
-        paddockPanels.add(paddockPanel1);
-        paddockPanels.add(paddockPanel2);
-        paddockPanels.add(paddockPanel3);
-        paddockPanels.add(paddockPanel4);
-        paddockPanels.add(paddockPanel5);
-        paddockPanels.add(paddockPanel6);
-        paddockPanels.add(paddockPanel7);
-        paddockPanels.add(paddockPanel8);
 
         // Listeners
         visitStoreButton.addActionListener(new ActionListener() {
@@ -96,24 +61,37 @@ public class MainScreen {
                 System.out.println("End Game");
             }
         });
+        nextButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                changeSelectedSlot(1);
+                updateItemNumberLabel();
+            }
+        });
+        previousButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                changeSelectedSlot(-1);
+                updateItemNumberLabel();
+            }
+        });
     }
 
     /**
-     * Draw the items in the paddock panel
-     * Draws out everything in the farm paddock items array
+     * Update the item number label to
+     * "Showing Paddock Slow currentslot+1/9"
      */
-    private void drawPaddockPanel() {
-        for (int i = 0; i < 9; i ++) {
-//            FarmItem item = Game.getFarm().getPaddockItems().get(i);
-            JLabel label = new JLabel("HOES");
-            paddockPanels.get(i).add(label);
-        }
+    public void updateItemNumberLabel() {
+        itemNumberLabel.setText("Showing Paddock Slot " + (currentSlot + 1) + "/9");
     }
 
     /**
-     * Draw the item onto the paddock panel
+     * Increase the selected slot number by change
+     * Clamps it between 0 and 8 (inclusive)
+     * @param change
      */
-    private void drawPaddockItem() {
+    public void changeSelectedSlot(int change) {
+        currentSlot = Math.max(Math.min(currentSlot + change, 8), 0);
     }
 
     /**
@@ -133,22 +111,7 @@ public class MainScreen {
         Game.getFarm().addFarmItem(new Sheep());
         Game.getFarm().addFarmItem(new SugarCane());
 
-        drawPaddockPanel();
         frame.pack();
-    }
-
-    private void createUIComponents() {
-
-        // Paddock Panels
-        paddockPanel0 = new JPanel();
-        paddockPanel1 = new JPanel();
-        paddockPanel2 = new JPanel();
-        paddockPanel3 = new JPanel();
-        paddockPanel4 = new JPanel();
-        paddockPanel5 = new JPanel();
-        paddockPanel6 = new JPanel();
-        paddockPanel7 = new JPanel();
-        paddockPanel8 = new JPanel();
     }
 }
 
